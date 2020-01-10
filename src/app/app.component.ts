ed21ed29e0c1b2f18483d5000631805c310205d7
Carlos Navarro <carlos.navarro@eimprovement.com>
2020-01-10 00:33:34 -0600

    feat(app): add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServerService } from './shared/services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular8-dynamic-components';
+export class AppComponent implements OnInit {
+
+  //#region   Public Methods
+  title = 'Angular SSR and Dynamic Components';
+  //#endregion
+
+  //#region   Private Properties
+  private alive = true;
+  //#endregion
+
+  //#region   Constructor
+  constructor(private serverService: ServerService) { }
+  //#endregion
+
+  //#region   Lifecycle Hooks
+  ngOnInit(): void {
+    this.serverService.getUsers().subscribe(users => this.serverService.users$.next(users));
+  }
+  //#endregion
+
 }
