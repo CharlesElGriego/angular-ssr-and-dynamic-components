6d8cfc726246c8ce2c62ea30640962609cb5cd99
Alexis Villegas <avillegas@ex2outcoding.local>
2020-01-10 10:36:23 -0600

    feat: add no server side rendering directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,41 @@
 import { HttpClientModule } from '@angular/common/http';
-import { Compiler, CompilerFactory, COMPILER_OPTIONS, NgModule } from '@angular/core';
+import { Compiler, COMPILER_OPTIONS, CompilerFactory, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { JitCompilerFactory } from '@angular/platform-browser-dynamic';
 import { RouterModule } from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { NoSSRDirective } from './core/directives/no-ssr.directive';
 import { PipesModule } from './shared/pipes';
 import { SafeurlPipe } from './shared/pipes/safeurl.pipe';
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent, NoSSRDirective],
   imports: [
     AppRoutingModule,
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserModule.withServerTransition({ appId: "serverApp" }),
     HttpClientModule,
     RouterModule,
     CoreModule,
     PipesModule
   ],
-  providers: [SafeurlPipe,
+  providers: [
+    SafeurlPipe,
     // Compiler is not included in AOT-compiled bundle.
     // Must explicitly provide compiler to be able to compile templates at runtime.
     { provide: COMPILER_OPTIONS, useValue: {}, multi: true },
-    { provide: CompilerFactory, useClass: JitCompilerFactory, deps: [COMPILER_OPTIONS] },
+    {
+      provide: CompilerFactory,
+      useClass: JitCompilerFactory,
+      deps: [COMPILER_OPTIONS]
+    },
     { provide: Compiler, useFactory: createCompiler, deps: [CompilerFactory] }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
 export function createCompiler(compilerFactory: CompilerFactory) {
   return compilerFactory.createCompiler();
 }
---
+++ src/app/core/directives/no-ssr.directive.ts
@@ -0,0 +1,25 @@
+import { isPlatformServer } from '@angular/common';
+import { Directive, Inject, OnInit, PLATFORM_ID, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: "[noSSR]"
+})
+export class NoSSRDirective implements OnInit {
+  constructor(
+    private viewContainer: ViewContainerRef,
+    private templateRef: TemplateRef<any>,
+    @Inject(PLATFORM_ID) private platformId
+  ) {}
+
+  ngOnInit(): void {
+    if (isPlatformServer(this.platformId)) {
+      // render on express render
+      console.log("server side rendered");
+      this.viewContainer.clear();
+    } else {
+      // this will work on the client
+      console.log("client side rendered");
+      this.viewContainer.createEmbeddedView(this.templateRef);
+    }
+  }
+}
